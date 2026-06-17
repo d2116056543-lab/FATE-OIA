36d591d7520a5c9ab9ca09e3933838300d4f9208
WLJTXY <d2116056543@gmail.com>
2026-06-17 23:14:07 +0800

    Fix ACPR-PMT-S foreground review-pass switch

--- scripts/FATE_OIA_acpr_pmt_s_v1_foreground.ps1
+++ scripts/FATE_OIA_acpr_pmt_s_v1_foreground.ps1
@@ -7,11 +7,16 @@ param(
 )
 $ErrorActionPreference = "Stop"
 Set-Location E:\sbw\FATE_Drive\fate_oia_acpr_pmt_s_v1_worktree
-E:\Anaconda\envs\sbw39\python.exe -u -m fate_oia.engine.supervise_acpr_pmt_s_foreground `
-  --config configs\fate_oia_train_360x640_acpr_pmt_s_v1.yaml `
-  --epochs $Epochs `
-  --batch_size $BatchSize `
-  --gradient_accumulation_steps $GradAccum `
-  --device $Device `
-  --output_dir .background_runs\acpr_pmt_s_v1_360x640_testonly_18e `
-  --require_review_pass:$RequireReviewPass
+$argsList = @(
+  "-u", "-m", "fate_oia.engine.supervise_acpr_pmt_s_foreground",
+  "--config", "configs\fate_oia_train_360x640_acpr_pmt_s_v1.yaml",
+  "--epochs", "$Epochs",
+  "--batch_size", "$BatchSize",
+  "--gradient_accumulation_steps", "$GradAccum",
+  "--device", "$Device",
+  "--output_dir", ".background_runs\acpr_pmt_s_v1_360x640_testonly_18e"
+)
+if ($RequireReviewPass) {
+  $argsList += "--require_review_pass"
+}
+& E:\Anaconda\envs\sbw39\python.exe @argsList
